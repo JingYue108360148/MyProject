a6ae796ae0d6e00d770615f8681c361c02324b3d
JingYue108360148 <t108360148@ntut.org.tw>
2021-10-20 15:04:06 +0800

    first

---
+++ idea_test/src/Homework_CH1/test_p25.java
@@ -0,0 +1,12 @@
+package Homework_CH1;
+
+public class test_p25 {
+    public static void main(String[]args)
+    {
+        int num;
+
+        num=3;
+
+        System.out.println("變數num的數值是"+num);
+    }
+}
